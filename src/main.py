b7530a51dda23c74e45b753b80d48fc392d44f1d
Grace Kim <gracek1459@gmail.com>
2025-01-21 22:36:12 -0800

    made the correct changes for the free_select folder file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import brahe as bh
 from common.utils import load_earth_data #,compute_all_gaps_contacts, compute_earth_interior_angle
 from common.sat_gen import satellites_from_constellation
-from common.objective_functions import cost_func_gap
 from common.plotting import plot_gif
+from methods.free_select.nelder_mead_scipy import nelder_mead_scipy
 
-from scipy.optimize import minimize
 import numpy as np
 
 # Hydra
@@ -51,31 +50,9 @@ def main(cfg: DictConfig):
     # TODO: add other methods
     if cfg.problem.type == "free":
         if cfg.problem.method == "nelder":
-    
-            # Initial guess for the coordinates (x, y) 
-            # Overridden currently by initial simplex
-            initial_guess = np.array([-30, -60])
-
-            # TODO: Find best initial simplex to start, may need to change based on continents
-            initial_simplex =  np.array([[-30, -60],[-100, -60],[-30, -90]])
             
-            # Additional args for function
-            gs_list = []
-            sat_list = satellites=satellites[0:cfg.problem.sat_num]
-            land_geometries = land_data['geometry']
-            plot = False
+            nelder_mead_scipy(cfg,land_data,global_list_of_simplexes,epc_start,epc_end,satellites)
 
-            # Perform the optimization using Nelder-Mead
-            result = minimize(cost_func_gap, 
-                            initial_guess, 
-                            args = (gs_list, global_list_of_simplexes, sat_list, epc_start, epc_end, land_geometries, plot), 
-                            method='Nelder-Mead',
-                            options={'disp': True,
-                                    'maxiter': 5,
-                                    'initial_simplex': initial_simplex},
-                            bounds = ((-180,180),(-90,90)))
-            
-            print(result)
             plot_gif(global_list_of_simplexes)
             print(global_list_of_simplexes)
 
--- src/methods/free_select/nelder_mead_scipy.py
+++ src/methods/free_select/nelder_mead_scipy.py
@@ -1,20 +1,30 @@
+import numpy as np
+from scipy.optimize import minimize
+from common.objective_functions import cost_func_gap
 
-# List to store simplex points
-simplex_history = []
+# TODO: add the cyclic coordinate descent part
+def nelder_mead_scipy(cfg,land_data,global_list_of_simplexes,epc_start,epc_end,satellites):
 
-def callback(xk, convergence=None):
-    simplex_history.append(xk.copy())
+    # Initial guess for the coordinates (x, y) 
+    # Overridden currently by initial simplex
+    initial_guess = np.array([-30, -60])
 
-# Initial guess for the coordinates (x, y)
-initial_guess = np.array([-30, -60])
+    # TODO: Find best initial simplex to start, may need to change based on continents
+    initial_simplex =  np.array([[-30, -60],[-100, -60],[-30, -90]])
+    
+    # Additional args for function
+    gs_list = []
+    sat_list = satellites[0:cfg.problem.sat_num]
+    land_geometries = land_data['geometry']
+    plot = False
 
-# Perform the optimization using Nelder-Mead
-result = minimize(cost_func_gap, 
-                  initial_guess, 
-                  method='Nelder-Mead',
-                  options={'disp': True,
-                          'maxiter': 5,
-                           'initial_simplex' : np.array([[-30, -60],[-100, -60],[-30, -90]])},
-                  callback=callback,
-                  bounds = ((-180,180),(-90,90)))
-
+    # Perform the optimization using Nelder-Mead
+    result = minimize(cost_func_gap, 
+                    initial_guess, 
+                    args = (gs_list, global_list_of_simplexes, sat_list, epc_start, epc_end, land_geometries, plot), 
+                    method='Nelder-Mead',
+                    options={'disp': True,
+                            'maxiter': 5,
+                            'initial_simplex': initial_simplex},
+                    bounds = ((-180,180),(-90,90)))
+    
