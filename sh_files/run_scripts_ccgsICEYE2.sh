a731d5fb0cfada5cb7396085e36a22c631b964ae
Grace <gracek1459@gmail.com>
2025-05-07 14:48:02 -0700

    sh files from bethpage

---
+++ sh_files/run_scripts_ccgsICEYE2.sh
@@ -0,0 +1,12 @@
+# python src/main.py --multirun problem.gs_num=1,2,3,4,5,7,10,15,20 debug.randseed=0,1,2,3,4
+
+# python src/main.py --multirun scenario.start_simplex=0,1,2,3,4,5,6
+
+for gs_num in 7; do
+  for randseed in 0 1 2 3 4; do
+    python src/main.py problem.gs_num=$gs_num debug.randseed=$randseed problem.method="nelder_ccgs" problem.sat_num=34 scenario.constellations="ICEYE"
+    sleep 3
+  done
+done
+
+echo "All scripts executed."
